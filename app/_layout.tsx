db5d6344c2c583407f4f5ea834dc2dfa90887b54
Yuria Loo <yuria.loo@comcast.net>
2026-03-04 14:05:16 -0800

    Cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,22 +2,13 @@ import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+} from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
 
-import { PuzzleProvider } from '@/contexts/PuzzleContext';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-}
-SplashScreen.setOptions({
-  duration: 1000,
-  fade: true,
-});
+import { PuzzleProvider } from '@/contexts/PuzzleContext'
+import { useColorScheme } from '@/hooks/use-color-scheme'
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
