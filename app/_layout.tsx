231859889b6a8633714f8074af28d1051d8608ed
Yuria Loo <yuria.loo@comcast.net>
2026-03-03 18:35:19 -0800

    Add splash import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,22 @@ import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from '@react-navigation/native'
-import { Stack } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
-import 'react-native-reanimated'
+} from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 
-import { PuzzleProvider } from '@/contexts/PuzzleContext'
-import { useColorScheme } from '@/hooks/use-color-scheme'
+import { PuzzleProvider } from '@/contexts/PuzzleContext';
+import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 }
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
@@ -21,6 +26,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <PuzzleProvider>
         <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
         <StatusBar style="auto" />
