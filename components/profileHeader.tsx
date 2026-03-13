50168097fde71386b4499b0c0c038738cb2428b7
Ann N <jovyannnelson@gmail.com>
2026-03-13 15:23:15 -0700

    Fix prettier

--- components/profileHeader.tsx
+++ components/profileHeader.tsx
@@ -1,8 +1,16 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as ImagePicker from 'expo-image-picker';
-import React, { useState } from 'react';
-import { Image, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as ImagePicker from 'expo-image-picker'
+import React, { useState } from 'react'
+import {
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 
 /**
  * @typedef {Object} ProfileHeaderProps
@@ -20,255 +28,265 @@ import { Image, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } fro
  * @param {ProfileHeaderProps} props - The props parameter for profile header
  * @returns {JSX.Element} The profile header
  */
-export function ProfileHeader({ avatarUri, setAvatarUri, firstName, setFirstName, lastName, setLastName, streak }: any) {
-    // Local initial state for editing first and last name.
-    const [tempFirst, setTempFirst] = useState(firstName);
-    const [tempLast, setTempLast] = useState(lastName);
+export function ProfileHeader({
+  avatarUri,
+  setAvatarUri,
+  firstName,
+  setFirstName,
+  lastName,
+  setLastName,
+  streak,
+}: any) {
+  // Local initial state for editing first and last name.
+  const [tempFirst, setTempFirst] = useState(firstName)
+  const [tempLast, setTempLast] = useState(lastName)
 
-    // Local initial state for modal visibility
-    const [modalVisible, setModalVisible] = useState(false);
+  // Local initial state for modal visibility
+  const [modalVisible, setModalVisible] = useState(false)
 
-    // Fallback to Avatar Initials (If avatar image is removed)
-    const initials = `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
+  // Fallback to Avatar Initials (If avatar image is removed)
+  const initials = `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase()
 
-    // function to launch Image Picker.
-    const pickImage = async () => {
-        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    
-        if (permissionResult.granted === false) {
-          alert("You need to allow this app to access your photos")
-          return;
-        }
-    
-        const result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ['images'],
-          allowsEditing: true,
-          aspect: [1, 1],
-          quality: 1,
-        });
-    
-        // If image is selected, updates state and persist URI to AsyncStorage
-        if (!result.canceled) {
-          const uri = result.assets[0].uri;
-          setAvatarUri(uri);
-          await AsyncStorage.setItem('@user_avatar', uri);
-        }
-    };
+  // function to launch Image Picker.
+  const pickImage = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync()
 
-    // function to remove avatar
-    const handleRemoveAvatar = async () => {
-        setAvatarUri(null);
-        await AsyncStorage.removeItem('@user_avatar');
+    if (permissionResult.granted === false) {
+      alert('You need to allow this app to access your photos')
+      return
     }
 
-    // function to open the edit modal
-    const handleOpenModal = () => {
-        setTempFirst(firstName);
-        setTempLast(lastName);
-        setModalVisible(true);
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    })
+
+    // If image is selected, updates state and persist URI to AsyncStorage
+    if (!result.canceled) {
+      const uri = result.assets[0].uri
+      setAvatarUri(uri)
+      await AsyncStorage.setItem('@user_avatar', uri)
     }
+  }
 
-    // function to save first and last name
-    const handleSave = async () => {
-        setFirstName(tempFirst);
-        setLastName(tempLast);
+  // function to remove avatar
+  const handleRemoveAvatar = async () => {
+    setAvatarUri(null)
+    await AsyncStorage.removeItem('@user_avatar')
+  }
 
-        try {
-            await AsyncStorage.setItem('@first_name', tempFirst);
-            await AsyncStorage.setItem('@last_name', tempLast);
-            
-        } catch (e) {
-            console.error ("Error saving name", e);
-        }
-        setModalVisible(false);
-    };
+  // function to open the edit modal
+  const handleOpenModal = () => {
+    setTempFirst(firstName)
+    setTempLast(lastName)
+    setModalVisible(true)
+  }
 
-    return (
-        <View style={styles.userRow}>
-            {/* AVATAR */}
-            <TouchableOpacity style ={styles.avatar} onPress={pickImage}>
-                {avatarUri ? (
-                    <Image source= {{ uri: avatarUri }} style={styles.avatarImage} />
-                ) : (
-                    <Text style={styles.initials}>{initials}</Text>
-                )}
-            </TouchableOpacity>
+  // function to save first and last name
+  const handleSave = async () => {
+    setFirstName(tempFirst)
+    setLastName(tempLast)
 
-            {/* FIRST AND LAST NAME */}
-            <View>
-                <View style={styles.nameRow}>
-                    <TouchableOpacity onPress={handleOpenModal}>
-                        <Text>
-                            <Text style={styles.usernameInput}>{firstName} {lastName} </Text>
-                            <Ionicons name="pencil-outline" size={16} />
-                        </Text>
-                    </TouchableOpacity>
-                </View>
+    try {
+      await AsyncStorage.setItem('@first_name', tempFirst)
+      await AsyncStorage.setItem('@last_name', tempLast)
+    } catch (e) {
+      console.error('Error saving name', e)
+    }
+    setModalVisible(false)
+  }
 
-                {/* CURRENT DAY STREAK */}
-                <Text style={styles.streak}>{streak} day streak</Text>
-            </View>
+  return (
+    <View style={styles.userRow}>
+      {/* AVATAR */}
+      <TouchableOpacity style={styles.avatar} onPress={pickImage}>
+        {avatarUri ? (
+          <Image source={{ uri: avatarUri }} style={styles.avatarImage} />
+        ) : (
+          <Text style={styles.initials}>{initials}</Text>
+        )}
+      </TouchableOpacity>
 
-            {/* MODAL */}
-            <Modal
-                animationType="fade"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                    setModalVisible(false);
-                }}
-            >
-                <View style={styles.modalOverlay}>
-                    <View style={styles.modalContent}>
-                        <Text style={styles.heading}>Edit Profile</Text>
-
-                        <TextInput
-                            style={styles.modalInput}
-                            value={tempFirst}
-                            onChangeText={setTempFirst}
-                            placeholder="FirstName"
-                            placeholderTextColor={'#999'}
-                        />
-                        <TextInput
-                            style={styles.modalInput}
-                            value={tempLast}
-                            onChangeText={setTempLast}
-                            placeholder="LastName"
-                            placeholderTextColor={'#999'}
-                        />
-
-                        {avatarUri && (
-                            <View style={{ width: '100%', margin: 15}}>
-                                <TouchableOpacity style={[styles.button, { flex: 0, alignItems: 'center' }]} onPress={handleRemoveAvatar}>
-                                    <Text style={styles.buttonText}> Remove Avatar</Text>
-                                </TouchableOpacity>
-                            </View>
-                        )}
-
-                        <View style={styles.buttonLayout}>
-                            <TouchableOpacity style={styles.button} onPress={() => setModalVisible(false)}>
-                                <Text style={styles.buttonText}>Cancel</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.button} onPress={handleSave}>
-                                <Text style={styles.buttonText}>Save</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-            </Modal>
+      {/* FIRST AND LAST NAME */}
+      <View>
+        <View style={styles.nameRow}>
+          <TouchableOpacity onPress={handleOpenModal}>
+            <Text>
+              <Text style={styles.usernameInput}>
+                {firstName} {lastName}{' '}
+              </Text>
+              <Ionicons name="pencil-outline" size={16} />
+            </Text>
+          </TouchableOpacity>
         </View>
-           
-    );
+
+        {/* CURRENT DAY STREAK */}
+        <Text style={styles.streak}>{streak} day streak</Text>
+      </View>
+
+      {/* MODAL */}
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false)
+        }}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <Text style={styles.heading}>Edit Profile</Text>
+
+            <TextInput
+              style={styles.modalInput}
+              value={tempFirst}
+              onChangeText={setTempFirst}
+              placeholder="FirstName"
+              placeholderTextColor={'#999'}
+            />
+            <TextInput
+              style={styles.modalInput}
+              value={tempLast}
+              onChangeText={setTempLast}
+              placeholder="LastName"
+              placeholderTextColor={'#999'}
+            />
+
+            {avatarUri && (
+              <View style={{ width: '100%', margin: 15 }}>
+                <TouchableOpacity
+                  style={[styles.button, { flex: 0, alignItems: 'center' }]}
+                  onPress={handleRemoveAvatar}
+                >
+                  <Text style={styles.buttonText}> Remove Avatar</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+
+            <View style={styles.buttonLayout}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={handleSave}>
+                <Text style={styles.buttonText}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  )
 }
 
 /**
  * StyleSheet for ProfileHeader
  */
 const styles = StyleSheet.create({
-    avatar: {
-        width: 80,
-        height: 80,
-        borderRadius: 40,
-        backgroundColor: '#F4D1C0',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: 12,
-        overflow: 'hidden'
-    },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: '#F4D1C0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 12,
+    overflow: 'hidden',
+  },
 
-    avatarImage: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover'
-    },
+  avatarImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
 
-    button: {
-        flex: 1,
-        backgroundColor: '#e87756',
-        padding: 12,
-        borderRadius: 12,
-        alignItems: 'center'
-    },
+  button: {
+    flex: 1,
+    backgroundColor: '#e87756',
+    padding: 12,
+    borderRadius: 12,
+    alignItems: 'center',
+  },
 
-    buttonLayout: {
-        flexDirection: 'row',
-        gap: 12
-    },
+  buttonLayout: {
+    flexDirection: 'row',
+    gap: 12,
+  },
 
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'semibold'
-    },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'semibold',
+  },
 
-    heading: {
-        fontSize: 20,
-        fontWeight: 'bold'
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 
-    },
+  initials: {
+    color: '#e87756',
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
 
-    initials: {
-        color: '#e87756',
-        textAlign: 'center',
-        fontSize:  24,
-        fontWeight: 'bold',
-    },
+  modalContent: {
+    width: '80%',
+    backgroundColor: '#fff',
+    padding: 24,
+    borderRadius: 20,
+    alignItems: 'center',
+    elevation: 5,
+  },
 
-    modalContent: {
-        width: '80%',
-        backgroundColor: '#fff',
-        padding: 24,
-        borderRadius: 20,
-        alignItems: 'center',
-        elevation: 5
+  modalInput: {
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    fontSize: 16,
+    paddingVertical: 8,
+    marginBottom: 20,
+    color: '#000',
+  },
 
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    },
+  nameRow: {
+    flexDirection: 'row',
+    gap: 4,
+  },
 
-    modalInput: {
-        width: '100%',
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        fontSize: 16,
-        paddingVertical: 8,
-        marginBottom: 20,
-        color: '#000'
+  streak: {
+    color: '#666',
+  },
 
-    },
+  username: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
 
-    modalOverlay: {
-        flex: 1,
-        backgroundColor: 'rgba(0,0,0,0.8)',
-        justifyContent: 'center',
-        alignItems: 'center'
+  usernameInput: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 20,
+    minWidth: 50,
+  },
 
-    },
-
-    nameRow: {
-        flexDirection: 'row',
-        gap: 4
-    },
-
-    streak: { 
-        color: '#666',
-    },
-
-    username: {
-        color: '#000',
-        fontWeight: 'bold',
-        fontSize: 20,
-    },
-
-    usernameInput: {
-        color: '#000',
-        fontWeight: 'bold',
-        fontSize: 20,
-        minWidth: 50
-    },
-
-    userRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        width: '100%'
-    },
-});
+  userRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+  },
+})
--- components/profileStat.tsx
+++ components/profileStat.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native'
 
 /**
  * @typedef {Object} ProfileStatProps
@@ -12,87 +12,89 @@ import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
  * @param {ProfileStatProps} props - The props parameter for profile stat
  * @returns {JSX.Element} The stats
  */
-export function ProfileStat( { streak, setStreak }: any) {
-    // local initial state for stats
-    const [puzzlesCompleted, setPuzzlesCompleted] = useState(100);
-    const [averageTime, setAverageTime] = useState('18:42');
-    const [bestStreak, setBestStreak] = useState(10);
+export function ProfileStat({ streak, setStreak }: any) {
+  // local initial state for stats
+  const [puzzlesCompleted, setPuzzlesCompleted] = useState(100)
+  const [averageTime, setAverageTime] = useState('18:42')
+  const [bestStreak, setBestStreak] = useState(10)
 
-    // function to reset stats
-    const handleResetStats = () => {
-        Alert.alert("Reset Stats", "Are you sure you want to clear your progress?",
-          [{ text: "Cancel", style: "cancel"},
-            { 
-                text: "Reset", 
-                style: "destructive", 
-                onPress: () => {
-                    setPuzzlesCompleted(0);
-                    setAverageTime('00:00');
-                    setBestStreak(0);
-                    setStreak(0);
-                },
-            },
-          ]
-        );
-    };
+  // function to reset stats
+  const handleResetStats = () => {
+    Alert.alert(
+      'Reset Stats',
+      'Are you sure you want to clear your progress?',
+      [
+        { text: 'Cancel', style: 'cancel' },
+        {
+          text: 'Reset',
+          style: 'destructive',
+          onPress: () => {
+            setPuzzlesCompleted(0)
+            setAverageTime('00:00')
+            setBestStreak(0)
+            setStreak(0)
+          },
+        },
+      ]
+    )
+  }
 
-    return (
+  return (
+    <View style={styles.statWrapper}>
+      <View style={styles.statsRow}>
+        <Text>Puzzles Completed</Text>
+        <Text style={styles.statValue}>{puzzlesCompleted}</Text>
+      </View>
 
-        <View style= {styles.statWrapper}>
-            <View style={styles.statsRow}>
-                <Text>Puzzles Completed</Text>
-                <Text style={styles.statValue}>{puzzlesCompleted}</Text>
-            </View>
+      <View style={styles.statsRow}>
+        <Text>Average Time</Text>
+        <Text style={styles.statValue}>{averageTime}</Text>
+      </View>
 
-            <View style={styles.statsRow}>
-                <Text>Average Time</Text>
-                <Text style={styles.statValue}>{averageTime}</Text>
-            </View>
+      <View style={styles.statsRow}>
+        <Text>Best Streak</Text>
+        <Text style={styles.statValue}>{bestStreak} days</Text>
+      </View>
 
-            <View style={styles.statsRow}>
-                <Text>Best Streak</Text>
-                <Text style={styles.statValue}>{bestStreak} days</Text>
-            </View>
-
-            <Pressable style={styles.button} onPress={handleResetStats}>
-                <Text style={styles.buttonText}>Reset Stats</Text>
-            </Pressable>
-        </View>
-    );
+      <Pressable style={styles.button} onPress={handleResetStats}>
+        <Text style={styles.buttonText}>Reset Stats</Text>
+      </Pressable>
+    </View>
+  )
 }
 
 /**
  * Stylesheet for ProfileStat
  */
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#e87756',
-        paddingVertical: 16,
-        paddingHorizontal: 80,
-        borderRadius: 16,
-        alignItems: 'center',
-        marginTop: 12
-    },
-    
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'semibold',
-        fontSize: 16
-    },
-
-    statsRow: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 12,
-        width: '100%'
-    },
-    
-    statValue: {
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-
-    statWrapper: {
-        width: '100%'
+  button: {
+    backgroundColor: '#e87756',
+    paddingVertical: 16,
+    paddingHorizontal: 80,
+    borderRadius: 16,
+    alignItems: 'center',
+    marginTop: 12,
   },
-});
+
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'semibold',
+    fontSize: 16,
+  },
+
+  statsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 12,
+    width: '100%',
+  },
+
+  statValue: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  statWrapper: {
+    width: '100%',
+  },
+})
